0d91fc5bbadc7de5d3118d620c89c117d5f7fce9
Milena Cardoso <mile.araujocar@gmail.com>
2025-09-04 20:17:53 -0300

    fix: update

--- hrpayroll/src/main/java/com/example/hrpayroll/Service/UserService.java
+++ hrpayroll/src/main/java/com/example/hrpayroll/Service/UserService.java
@@ -1,11 +1,13 @@
 package com.example.hrpayroll.Service;
 
+import com.example.hrpayroll.DTO.UserDTO;
 import com.example.hrpayroll.Model.UserModel;
 import com.example.hrpayroll.Repository.UserRepository;
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
@@ -29,17 +31,19 @@ public class UserService {
         public Optional<UserModel> findOneById(String id) {
                 return userRepository.findById(id);
         }
+        public UserModel updateUser (String id, UserDTO newUserData) {
+                Optional<UserModel> user = userRepository.findById(id);
+                
+                user.setNome(newUserData.getNome());
+                user.setEmail(newUserData.getEmail());
+                final S save = userRepository.save(newUserData);
+                return save;
+                
+        }
         public List<UserModel> delete(String id){
                 userRepository.findById(id);
                 return userRepository.findAll();
         }
 
-        public UserModel update(UserModel userModel) {
-                userRepository.save(userModel);
-        }
-        public List<UserModel> delete(String id) {
-                userRepository.deleteById(id);
-                return userRepository.findAll();
-        }
 
 }
